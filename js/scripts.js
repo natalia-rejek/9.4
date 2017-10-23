6f96b4b016d22fa88b5e6e4ec570c01fbde69479
Natalia <nettel@wp.pl>
2017-10-23 14:21:56 +0200

    sc commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+function drawTree(i){
+    
+	for(i = 1; i <= 5; i++){
+ 
+		var star = '';
+          
+		for(var j = 1; j <= i; j++){
+        
+			star = star + '*';
+      
+		}
+      
+		console.log(star);
+    
+	}  
+
+}
